a90070f3cbf01404d3c0c43ae76731b7f6e5bb84
Chi-Han Yang <hulayang@gmail.com>
2025-01-20 21:49:48 -0500

    Clean up frontend.

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,15 +1,19 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h1>About</h1>
+    <p>
+      This project is created to manage bill of materials (BOM) as a webapp. The frontend uses Vue3,
+      and the backend is Flask.
+    </p>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
+/* @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
     display: flex;
     align-items: center;
   }
-}
+} */
 </style>
